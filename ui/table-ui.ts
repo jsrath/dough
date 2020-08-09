07ea4b21953cbde3aa5bf0caae41065c39e8832d
Jeremy <jeremy@jsrath.com>
2020-08-09 11:36:40 +0200

    feat: add number formatting to table

--- ui/table-ui.ts
+++ ui/table-ui.ts
@@ -22,10 +22,23 @@ export class TableUI {
     Object.keys(data).forEach(key => {
       const selector: HTMLTableElement = document.querySelector(`.${this.getCategoryType(key)} tbody`);
       const row = selector.insertRow(-1);
-      data[key].forEach((cell, index) => row.insertCell(index).appendChild(document.createTextNode(cell.toString())));
+      data[key].forEach((cell, index) => row.insertCell(index).appendChild(document.createTextNode(this.formatNumber(cell))));
       const category = document.createTextNode(this.parseCategoryName(key));
       row.insertCell(0).appendChild(category);
+      const headerRow: HTMLTableElement = document.querySelector(`.${this.getCategoryType(key)} thead tr th`);
+      headerRow.setAttribute("colspan", (data[key].length + 1).toString());
     });
   }
 
+  formatNumber(input: number): string {
+    const decimalsRemoved = input.toLocaleString('en-US', { maximumFractionDigits: 0 });
+    if (input > 0) {
+      return decimalsRemoved;
+    }
+    return input < 0 ? this.replaceNegativesWithParentheses(decimalsRemoved) : "-";
+  }
+
+  replaceNegativesWithParentheses(input: string): string {
+    return `(${input.substring(1, input.length)})`;
+  }
 }
